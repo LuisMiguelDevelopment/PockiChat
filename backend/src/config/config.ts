4fe889de82c5585512d49cc3f55a9dbbf78c1310
Miguel Alvarez <luismiguelalvarezmarin109@gmail.com>
2025-02-07 20:57:27 -0500

    feat: :sparkles: se creo el archivo config.ts que configura las variables de entorno de la aplicacion

---
+++ backend/src/config/config.ts
@@ -0,0 +1,18 @@
+import { config } from "dotenv";
+
+/**
+ * Configura y obtiene las variables de entorno de la aplicación
+ * @example process.env.PORT
+ */
+
+config();
+
+const PORT = process.env.PORT ?? 3000;
+const DB_USER = process.env.DB_USER ?? "";
+const DB_PASSWORD = process.env.DB_PASSWORD ?? "";
+const DB_HOST = process.env.DB_HOST ?? "";
+const DB_DATABASE = process.env.DB_DATABASE ?? "";
+const DB_PORT = process.env.DB_PORT ?? "";
+
+
+export { PORT, DB_USER, DB_PASSWORD, DB_HOST, DB_DATABASE, DB_PORT };
